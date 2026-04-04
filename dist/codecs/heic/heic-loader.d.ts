80dd482713253ba69df0fe5ee72e98e14ac1b3f2
mayur19 <mayurdusane1@gmail.com>
2026-04-03 23:24:10 -0400

    chore: add .gitignore, remove node_modules from tracking

--- dist/codecs/heic/heic-loader.d.ts
+++
@@ -1,8 +0,0 @@
-import { Codec } from '../../types/codec';
-
-/**
- * Dynamically import and initialize the HEIC codec.
- * Used by the codec registry for lazy loading.
- */
-export declare function loadHeicCodec(): Promise<Codec>;
-//# sourceMappingURL=heic-loader.d.ts.map
--- dist/rendering/canvas-renderer.d.ts
+++
@@ -1,8 +0,0 @@
-import { DecodedImage } from '../types/image';
-import { RenderOptions } from '../types/options';
-
-/**
- * Render a DecodedImage onto a Canvas element.
- */
-export declare function renderToCanvas(canvas: HTMLCanvasElement, image: DecodedImage, options?: RenderOptions): void;
-//# sourceMappingURL=canvas-renderer.d.ts.map
